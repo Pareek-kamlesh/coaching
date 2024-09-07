ffa5e185be38f9bda786c2b3281e40ce2d5efa95
Pareek-kamlesh <kamleshpareek21@gmail.com>
2024-09-07 11:09:50 +0530

    resolved error

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,9 +1,8 @@
-// app/not-found.js
 export default function NotFound() {
     return (
       <div className="text-center py-12">
         <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
-        <p className="mt-4">Sorry, the page you are looking for doesn't exist.</p>
+        <p className="mt-4">Sorry, the page you are looking for doesn&apos;t exist.</p>
       </div>
     );
   }
